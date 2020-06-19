d44da831fe7e60e2100f76d596b89e0807385e81
“Didar” <enderoasis@gmail.com>
2020-06-20 00:35:18 +0600

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,64 +17,19 @@ $img = $result["message"]["photo"];
 $name = $result["message"]["from"]["username"];
 $keyboard = [["Выложить слот"],["Для ломбардов"],["Мой лот"]];
 
-if($text){
-    if ($text == "/start") {
-       $reply = "Добро пожаловать в бота!";
-       $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-       $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-   }elseif ($text == "/help") {
-       $reply = "Информация...";
-       $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-    // Название слота
-    }elseif ($text == "Выложить слот") {
-    $reply = "Введите название слота";
-    // Название слота
-    //$title = $result["message"]["text"];
-   // $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-
-   // $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => 'test' ]);
-    // Закрепление изображения
-    if(isset($title)) {
-        $reply = "Закрепите фотографию слота";
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]); }
-     elseif ($img) {
-
-        $file_id = $result[count($result) - 1]['file_id'];
-        $response = $telegram->getFile(['file_id' => $file_id]);
-        $linktoimg = $response['file_path'];
-        $url = 'https://api.telegram.org/file/bot' . $tkn . '/' . $linktoimg . '';
-        //Отправка на почту
-
-    } 
-        else {
-            // Если это не изображение
-            $this->sendMessage($chat_id, "Не понимаю команду! Просто загрузите картинку.");
-        
-        }
-   
-   
-   
-
-   //    $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
-   //    $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
-      
-    }elseif ($text == "Для ломбардов") {
+if (!empty($result['message']['text'])) {
+    $reply = "Добро пожаловать в бота!";
+    $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+	if (mb_stripos($text, '/start') !== false) {
+		sendTelegram(
+			'sendMessage', 
+			array(
+				'chat_id' => $chat_id,
+                'text' => $reply,
+                'reply_markup' => $reply_markup
+			)
+		);
  
-}elseif ($text == "Мой лот") {
-
-   }else{
-       $reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
-       $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
-   }
-}else{
-   $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
+		exit();	
+	} 
 }
-
-
-
-
-
-
-
-
-
