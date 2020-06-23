a07d113e86642424ee8b7b41b1e981ab2b178523
“Didar” <enderoasis@gmail.com>
2020-06-23 18:28:20 +0600

    Update getUpdatesCLI.php

--- getUpdatesCLI.php
+++ getUpdatesCLI.php
@@ -5,12 +5,7 @@ require __DIR__ . '/vendor/autoload.php';
 $bot_api_key  = '1277709938:AAF4Bkt4ULyVrxQWp8f0yfHR-2DDSWwC38A';
 $bot_username = 'SmartPrototype_bot';
 
-$mysql_credentials = [
-   'host'     => 'srv-db-plesk01.ps.kz:3306',
-   'user'     => 'lombardb_admin',
-   'password' => 'S^r07si0',
-   'database' => 'lombardb_storage',
-];
+
 use Longman\TelegramBot\Telegram;
 use Longman\TelegramBot\TelegramLog;
 
@@ -19,7 +14,9 @@ try {
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
 
     // Enable MySQL
-    $telegram->enableMySql($mysql_credentials);
+    $telegram->enableExternalMySql($external_pdo_connection);
+
+    $external_pdo_connection = new PDO('mysql:host=srv-db-plesk01.ps.kz;dbname=lombardb_storage;port=3306','lombardb_admin','S^r07si0');
 
     // Handle telegram getUpdates request
     $telegram->handleGetUpdates();
