f1e69950a2b1a6cf43cadb937d98510d1d25dc56
“Didar” <enderoasis@gmail.com>
2020-06-27 03:04:09 +0600

    Update RetrieveCommand.php

--- Commands/RetrieveCommand.php
+++ Commands/RetrieveCommand.php
@@ -54,18 +54,21 @@ class RetrieveCommand extends UserCommand
     $db = new PDO('mysql:host=srv-db-plesk01.ps.kz:3306;dbname=lombardb_telegrambot', $user, $pass);
 
     // Делаем выборку из таблицы лотов
-    $stmt = $db->query("SELECT `notes` FROM `conversation`")->fetchAll(PDO::FETCH_ASSOC);;
-   
-    foreach ($stmt as $k => $v){
+    $stmt = $db->prepare("SELECT `id`,`notes` FROM `conversation` ");
+    $stmt->execute();
+    if($stmt->rowCount() > 0){
+        while($res = $stmt->fetch(PDO::FETCH_BOTH)){
+    
 
     $lots = [
         'chat_id'      => $this->getMessage()->getChat()->getId(),
-        'text'         => $v['notes']
+        'text'         => $res['notes']
     ];
 
     return Request::sendMessage($lots);
 
     }
+}
 
 
 
