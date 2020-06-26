7bf06db63323407206e1087cfcf6edf182db11c9
“Didar” <enderoasis@gmail.com>
2020-06-27 01:59:54 +0600

    Update RetrieveCommand.php

--- Commands/RetrieveCommand.php
+++ Commands/RetrieveCommand.php
@@ -60,7 +60,7 @@ class RetrieveCommand extends UserCommand
 
     $lots = [
         'chat_id'      => $this->getMessage()->getChat()->getId(),
-        'text'         => $v['notes']
+        'text'         => $v
     ];
 
     return Request::sendMessage($lots);
