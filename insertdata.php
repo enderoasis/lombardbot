318ce6d3c723c3f6d5bac505dd46ecc302af8953
“Didar” <enderoasis@gmail.com>
2020-07-05 17:25:50 +0600

    Update insertdata.php

--- insertdata.php
+++ insertdata.php
@@ -5,25 +5,15 @@ $tittle = $_POST['tittle'];
 $category = $_POST['category'];
 $email = $_POST['email'];
 $description = $_POST['description'];
+$image = file_get_contents( $_FILES['image']['tmp_name'] );
+$image = mysql_escape_string( $image );
 $user = 'lombardb_didar';
 $pass = '7likC9~2';
 $db = new PDO('mysql:host=srv-db-plesk01.ps.kz:3306;dbname=lombardb_telegrambot', $user, $pass);
+$sql = "INSERT INTO goods (tittle, category, email, content ) VALUES (?,?,?,?,?)";
+$stmt= $db->prepare($sql);
+$stmt->execute([$tittle, $category, $email, $image, $description]);
+ 
 
-if( !empty( $_FILES['image']['name'] ) ) {
-  // Проверяем, что при загрузке не произошло ошибок
-  if ( $_FILES['image']['error'] == 0 ) {
-    // Если файл загружен успешно, то проверяем - графический ли он
-    if( substr($_FILES['image']['type'], 0, 5)=='image' ) {
-      // Читаем содержимое файла
-      $image = file_get_contents( $_FILES['image']['tmp_name'] );
-      // Экранируем специальные символы в содержимом файла
-      $image = mysql_escape_string( $image );
-      // Формируем запрос на добавление файла в базу данных
-      $sql = "INSERT INTO goods (tittle, category, email, content ) VALUES (?,?,?,?,?)";
-      $stmt= $db->prepare($sql);
-      $stmt->execute([$tittle, $category, $email, $image, $description]);
-   
-    }
-  }
-}
+
 ?>
