3f7dc5bfd36b3ad092abf3ec330ccd2cf4a89c86
“Didar” <enderoasis@gmail.com>
2020-07-06 05:27:06 +0600

    Update body.php

--- body.php
+++ body.php
@@ -1,6 +1,6 @@
 <?php
 use PDO;
- $user = 'lombardb_didar';
+    $user = 'lombardb_didar';
     $pass = '7likC9~2';
     $db = new PDO('mysql:host=srv-db-plesk01.ps.kz:3306;dbname=lombardb_telegrambot', $user, $pass);
 
@@ -25,21 +25,20 @@ use PDO;
 <section class="wrapper site-min-height">
   <h3><i class="fa fa-angle-right"></i> Список лотов</h3>
   <hr>
-  <div class="row mt">
-   
-    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
-
-      <div class="project-wrapper">
-        <div class="project">
-          <div class="photo-wrapper">
-            <div class="photo">
-              <a class="fancybox" href="<?=$path?>"><img class="img-responsive" src="<?=$path?>" alt=""></a>
-            </div>
-            <div class="overlay"></div>
-          </div>
-        </div>
-      </div>
+  <?php foreach($stmt as $row): ?>
+ 
+    <div style="padding:10px; margin-bottom:10px; border-bottom:#333 2px solid;">
+        <strong><?php echo  $row['tittle']; ?></strong><br />
+        <i>Автор: <?php echo $row['category']; ?></i> / 
+        <i>Дата публикации: <?php $row['date']; ?></i><br /><br />
+        <p><?php echo $row['description']; ?></p>
+        <p style="text-align:right; text-decoration:underline;">
+        <a class="fancybox" href="<?=$row['location']  ?>">
+            <img class="img-responsive" src="<?=$row['location']  ?>">Подробнее</a>
+        </p>
     </div>
+ 
+<?php endforeach; ?>
     <!-- col-lg-4 -->
    
   <!-- /row -->
