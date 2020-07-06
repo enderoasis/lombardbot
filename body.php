43014edc076db6ea2de373f7c76955b7bb4630c1
“Didar” <enderoasis@gmail.com>
2020-07-06 18:27:49 +0600

    Update body.php

--- body.php
+++ body.php
@@ -24,7 +24,7 @@ use PDO;
         <br>
         <p style="text-align:right; text-decoration:underline;">
          <a class="fancybox" href="<?=$row['location']  ?>">
-          <center>  <img class="img-responsive" style="height: 350px; width: auto;"  src="<?=$row['location']  ?>"></a> </center>
+          <center>  <img class="img-responsive"   src="<?=$row['location']  ?>"></a> </center>
         </p>
  <hr>
  
