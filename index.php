4951ec58c30bb437430761c339df3c484137f5f8
“Didar” <enderoasis@gmail.com>
2020-07-09 02:23:29 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title></title>
+        <title>Pawn.kz - Самые выгодные
+        предложения от ломбардов не выходя из дома</title>
         <meta name="description" content="Pawn.kz - Самые выгодные
         предложения от ломбардов не выходя из дома." /> 
 
