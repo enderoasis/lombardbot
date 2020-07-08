b30499f301387956313a02acb167e79f5101588a
“Didar” <enderoasis@gmail.com>
2020-07-08 20:13:20 +0600

    landing added

---
+++ index .php
@@ -0,0 +1,265 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title></title>
+        <meta name="description" content="Snap is a beautiful Bootstrap 4 template for multipurpose landing pages." /> 
+
+        <!--Inter UI font-->
+        <link href="https://rsms.me/inter/inter-ui.css" rel="stylesheet">
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Google Fonts -->
+<!-- Bootstrap core CSS -->
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
+
+        <!-- Bootstrap CSS / Color Scheme -->
+        <link rel="stylesheet" href="css/default.css" id="theme-color">
+    </head>
+    <body>
+   <section class="py-5 py-md-7" id="home">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 my-md-auto mb-5 mb-md-0 text-center text-md-left">
+                        <span class="text-uppercase text-muted small-xl">  pawn.kz - Предложения от
+        ломбардов</span>
+                        <h1 class="display-4 mt-2 mb-5">Самые выгодные
+        предложения<br> от ломбардов <br>не выходя из дома.</h1>
+                        <a href="publish.php" class="btn btn-primary d-inline-flex flex-row align-items-center">
+                            <em data-feather="shopping-bag" width="20" height="20" class="mr-2"></em>
+                            Выложить слот
+                        </a>
+                        <a href="#" class="btn btn-secondary d-md-inline-flex flex-row align-items-center d-none show-video">
+                            <em data-feather="play" width="20" height="20" class="mr-2"></em>
+                            Смотреть видео
+                        </a>
+                    </div>
+                    <div class="col-md-6 play-wrapper">
+                        <img src="img/macbook.png" class="img-fluid d-block mx-auto" alt="MacBook" />
+                        <div class="play d-flex flex-row align-item-center show-video">
+                            <em class="icon" data-feather="play"></em>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- features section -->
+ 
+
+        <!--  <section class="pb-6" id="about">
+            <div class="container">
+                <hr class="mt-5"/>
+                <div class="row my-5">
+                    <div class="col-md-6 order-2 order-md-1 my-md-auto">
+                        <h3>The ultimate video game maker.</h3>
+                        <p class="text-muted lead">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id ante posuere, vestibulum mauris eget, efficitur felis. Vestibulum tincidunt sit amet odio at gravida. Cras mollis dapibus orci, in interdum odio scelerisque rhoncus.
+                        </p>
+                        <a href="#" class="btn btn-primary">Learn more</a>
+                    </div>
+                    <div class="col-md-6 order-1 order-md-2">
+                        <img src="img/imac.png" class="img-fluid d-block mx-auto" alt="iMac"/>
+                    </div>
+                </div>
+
+                <div class="row mt-7">
+                    <div class="col-md-6">
+                        <img src="img/macbook.png" class="img-fluid d-block mx-auto" alt="MacBook"/>
+                    </div>
+                    <div class="col-md-6 my-md-auto">
+                        <h3>Trusted by the best video game makers.</h3>
+                        <p class="text-muted lead">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id ante posuere, vestibulum mauris eget, efficitur felis. Vestibulum tincidunt sit amet odio at gravida. Cras mollis dapibus orci, in interdum odio scelerisque rhoncus.
+                        </p>
+                        <a href="#" class="btn btn-primary">Learn more</a>
+                    </div>
+                </div>
+
+               
+            </div>
+        </section>-->
+       
+
+        <!--pricing section-->
+        <section class=" pt-6" id="pricing">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 mx-auto">
+                        <h2 class="dot-circle">Получить деньги просто.</h2>
+                        <p class="text-muted lead">Вы выставляете свое имущество. Мы находим самое выгодное предложение от ломбардов.</p>
+                        <div class="row pricing-table mt-5">
+                            <div class="col-sm-5">
+                                <ul class="list-unstyled pricing-list">
+                                    <li>
+                                        <h5>Первый шаг</h5>
+                                        <p class="text-muted">Выложить свой лот. Указать название, категорию , описание , прикрепить фото и опубликовать.</p>
+                                    </li>
+                                    <li>
+                                        <h5>Второй шаг</h5>
+                                        <p class="text-muted">Мы передаем ваш лот около сорока ломбардам и выбираем для вас самое выгодное условие.</p>
+                                    </li>
+                                    <li>
+                                        <h5>Третий шаг</h5>
+                                        <p class="text-muted">С вами связываются и делают предложение.</p>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-6 ml-auto bg-primary">
+                                <div class="card">
+                                                                       <img src="sc.png" style="height:730px;">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--help section-->
+        <section class=" mt-7" id="contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 mx-auto">
+                        <h2 class="dot-circle">Need help?</h2>
+                        <p class="text-muted lead">We'd love to answer your questions.</p>
+                        <div class="row mt-5 card-contacts">
+                            <div class="col-md-6 mb-3">
+                                <div class="card">
+                                    <div class="card-body text-center">
+                                        <div class="icon-box">
+                                            <div class="icon-box-inner small-xs text-primary">
+                                                <span data-feather="inbox" width="30" height="30"></span>
+                                            </div>
+                                        </div>
+                                        <h5>Send us an email</h5>
+                                        <p class="text-muted small-xl">We usually reply within 24 hours.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <div class="card">
+                                    <div class="card-body text-center">
+                                        <div class="icon-box">
+                                            <div class="icon-box-inner small-xs text-primary">
+                                                <span data-feather="message-circle" width="30" height="30"></span>
+                                            </div>
+                                        </div>
+                                        <h5>Chat with us</h5>
+                                        <p class="text-muted small-xl">The fastest way to contact us.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-5">
+                            <div class="col-md-6 mb-5">
+                                <h6>Can I try Snap for free?</h6>
+                                <p class="text-muted">Nam liber tempor cum soluta nobis eleifend option congue nihil imper per tem por legere me doming.</p>
+                            </div>
+                            <div class="col-md-6 mb-5">
+                                <h6>Do you have hidden fees?</h6>
+                                <p class="text-muted">Nam liber tempor cum soluta nobis eleifend option congue nihil imper per tem por legere me doming.</p>
+                            </div>
+                            <div class="col-md-6 mb-5">
+                                <h6>What are the payment methods you accept?</h6>
+                                <p class="text-muted">Nam liber tempor cum soluta nobis eleifend option congue nihil imper per tem por legere me doming.</p>
+                            </div>
+                            <div class="col-md-6 mb-5">
+                                <h6>How often do you release updates?</h6>
+                                <p class="text-muted">Nam liber tempor cum soluta nobis eleifend option congue nihil imper per tem por legere me doming.</p>
+                            </div>
+                            <div class="col-md-6 mb-5">
+                                <h6>What is your refund policy?</h6>
+                                <p class="text-muted">Nam liber tempor cum soluta nobis eleifend option congue nihil imper per tem por legere me doming.</p>
+                            </div>
+                            <div class="col-md-6 mb-5">
+                                <h6>How can I contact you?</h6>
+                                <p class="text-muted">Nam liber tempor cum soluta nobis eleifend option congue nihil imper per tem por legere me doming.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--footer / contact-->
+        <footer class="mt-5 mb-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 my-auto text-center text-md-left">
+                        &copy;2020 pawn.kz
+                    </div>
+                    <div class="col-md-4 text-center">
+                        <ul class="list-inline social social-white-alt social-rounded social-sm mb-0">
+                            <li class="list-inline-item">
+                                <a href=""><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href=""><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href=""><i class="fa fa-google-plus"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href=""><i class="fa fa-dribbble"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 text-center text-md-right mb-0">
+                        <ul class="list-inline">
+                            <li class="list-inline-item"><a href="#">Privacy</a></li>
+                            <li class="list-inline-item"><a href="#">Terms</a></li>
+                            <li class="list-inline-item"><a href="#">Affiliates</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="col-12 text-muted text-center small">
+                        &copy; 2020 pawn.kz - Все права защищены
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <!--video section-->
+        <section class="video-wrapper">
+            <div class="video-close" id="video-close">
+                <em data-feather="x" class="icon"></em>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto my-auto">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item allowfullscreen" 
+                                    src="https://www.youtube.com/embed/MGsalg2f9js?rel=0&amp;controls=0&amp;showinfo=0" 
+                                    allowfullscreen="" frameborder="0" height="415" width="100%">
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--scroll to top-->
+        <div class="scroll-top">
+            <i class="fa fa-angle-up" aria-hidden="true"></i>
+        </div>
+
+        <!-- theme switcher (FOR DEMO ONLY - REMOVE FROM PRODUCTION)-->
+      
+
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.5.0/feather.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
