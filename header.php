e9a4a526f92c88747ff651367249fad3a09bc824
“Didar” <enderoasis@gmail.com>
2020-07-09 01:15:22 +0600

    fix

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
         <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
       </div>
       <!--logo start-->
-      <a href="index.php" class="logo"><b>Lombard<span>Bot</span></b></a>
+      <a href="board.php" class="logo"><b>Pawn<span>.Kz</span></b></a>
       <!--logo end-->
      
          
