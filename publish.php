120a81d2ca2973d6108bba5dea84aae38184ab59
“Didar” <enderoasis@gmail.com>
2020-07-09 01:47:08 +0600

    Update publish.php

--- publish.php
+++ publish.php
@@ -72,18 +72,7 @@ function mask(event) {
         TOP BAR CONTENT & NOTIFICATIONS
         *********************************************************************************************************************************************************** -->
     <!--header start-->
-    <header class="header black-bg">
-      <div class="sidebar-toggle-box">
-        <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
-      </div>
-      <!--logo start-->
-      <!--logo end-->
-      
-      <div class="top-menu">
-        <ul class="nav pull-right top-menu">
-        </ul>
-      </div>
-    </header>
+   
     <!--header end-->
     <!-- **********************************************************************************************************************************************************
         MAIN SIDEBAR MENU
